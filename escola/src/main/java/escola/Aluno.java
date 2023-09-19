e85b07ffbebc854912e3f56eeaefa70df9cc0737
genaton <genaton@gmail.com>
2023-09-19 19:42:08 -0300

    ciraClasseAluno

---
+++ escola/src/main/java/escola/Aluno.java
@@ -0,0 +1,14 @@
+// Modelando a classe Aluno
+
+package escola;
+
+public class Aluno {
+    
+    private String cpf; // segue padrao entidade pois eh um atributo unico
+    private String nome;
+    private Email email; // desafio criar uma classe email
+
+
+
+
+}
